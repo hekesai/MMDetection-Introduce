02c1995d0e5e3a4553f6b11d0b87aa60e3e991bf
hekesai <13199561860@163.com>
2019-07-09 15:13:51 +0800

    add test_inferece_time.py

--- mmdetection-inference/test_inference_time.py
+++ mmdetection-inference/test_inference_time.py
@@ -1,3 +1,31 @@
-#!/usr/bin/env python
-# coding=utf-8
+from mmdet.apis import init_detector,inference_detector,show_result
+import os
+import time
+import mmcv
 
+def test_inference(config_file,model_file):
+
+    model = init_detector(config_file,model_file,device='cuda:0')
+
+    g_loop_count = 100
+    image = 'test.jpg'
+    input_image = mmcv.imread(image)
+
+    start_time = time.time()
+
+    for i in range(g_loop_count):
+        result = inference_detector(model,input_image)
+
+    end_time = time.time()
+
+    avg_time = (end_time-start_time) / g_loop_count
+
+    return avg_time
+
+if __name__ == '__main__':
+    config_file = 'zhuoshi-train/cascade_rcnn_x101_64x4d_fpn_1x.py'
+    model_file = 'zhuoshi-train/epoch_4.pth'
+
+    inference_time = test_inference(config_file,model_file)
+    print('model inference cost %f',inference_time)
+    
